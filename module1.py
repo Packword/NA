c10639e6c428c163cf546cf2f8cc2a865a02bd8f
Packword <40355156+Packword@users.noreply.github.com>
2019-03-14 22:20:13 +0600

    Delete module1.py

--- module1.py
+++
@@ -1,9 +0,0 @@
-def Uscor (V,VE,T):
-    print((VE-V)/T)
-def Decorate(Uscor,V,VE,T):
-    def wrapper():
-        Uscor(V,VE,T)
-        print('S= ', ((V+VE)/2)*T)
-    return wrapper
-Uscor=Decorate(Uscor,2,4,2)
-Uscor()
